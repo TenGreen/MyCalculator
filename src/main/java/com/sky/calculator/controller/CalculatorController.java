55817ca028a269193eb35dc0967a1587d0fd7821
Ten-Green <GU27081975@yandex.ru>
2023-06-01 00:25:38 +0300

    Почистил, добавил проверку деления на ноль

--- src/main/java/com/sky/calculator/controller/CalculatorController.java
+++ src/main/java/com/sky/calculator/controller/CalculatorController.java
@@ -39,7 +39,10 @@ public class CalculatorController {
 
     @GetMapping("/calculator/divide")
     public String divide(@RequestParam("num1") int a, @RequestParam("num2") int b) {
-        int result = calculatorService.multiply(a, b);
+        if (b == 0) {
+            return "На ноль делить нельзя";
+        }
+        int result = calculatorService.divide(a, b);
         return String.format("%s / %s = %s", a, b, result);
     }
 }
